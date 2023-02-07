16ed0a2b8283f4eee6562d180162fec93ce102cb
mintchocoicecream <sxxhnjng@gmail.com>
2023-02-07 21:51:10 +0900

    updateBoard
    addNol.zip&Jal.zip Board,
    add BoardNav event

---
+++ js/board.js
@@ -0,0 +1,36 @@
+var nav=document.querySelector(".boardNav");
+var mukNav=nav.querySelector("span:first-child>a");
+var nolNav=nav.querySelector("span:nth-child(2)>a");
+var jalNav=nav.querySelector("span:last-child>a");
+var mukDiv=document.querySelector(".boardMuk");
+var nolDiv=document.querySelector(".boardPlay");
+var jalDiv=document.querySelector(".boardJal");
+
+var className="currentNav";
+var hiddenClass="hidden";
+
+mukNav.addEventListener('click', function(){
+        mukNav.classList.add(className);
+        nolNav.classList.remove(className);
+        jalNav.classList.remove(className);
+        mukDiv.classList.remove(hiddenClass);
+        nolDiv.classList.add(hiddenClass);
+        jalDiv.classList.add(hiddenClass);
+});
+nolNav.addEventListener('click', function(){
+        mukNav.classList.remove(className);
+        nolNav.classList.add(className);
+        jalNav.classList.remove(className);
+        mukDiv.classList.add(hiddenClass);
+        nolDiv.classList.remove(hiddenClass);
+        jalDiv.classList.add(hiddenClass);
+});
+jalNav.addEventListener('click', function(){
+        mukNav.classList.remove(className);
+        nolNav.classList.remove(className);
+        jalNav.classList.add(className);
+        mukDiv.classList.add(hiddenClass);
+        nolDiv.classList.add(hiddenClass);
+        jalDiv.classList.remove(hiddenClass);
+});
+
